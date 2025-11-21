243953859488dbe1da2d6c472cd0627ed5c8f64d
Andrey Arama <zekmaro@yandex.ru>
2025-11-21 14:45:08 +0100

    feat: leetcode is fucked up thing

---
+++ unique_len_3_palindromic_subseq/main.py
@@ -0,0 +1,26 @@
+class Solution:
+	def countPalindromicSubsequence(self, s: str) -> int:
+		n = len(s)
+		polyndroms = set()
+		left = set()
+		right = {}
+
+		for c in s:
+			if c not in right:
+				right[c] = 1
+			else:
+				right[c] += 1
+
+		for m in s:
+			right[m] -= 1
+			for c in left:
+				if right[c] > 0:
+					polyndroms.add((c, m))
+			left.add(m)
+		
+		return len(polyndroms)
+
+
+sol = Solution()
+s = "aabca"
+print(sol.countPalindromicSubsequence(s))
